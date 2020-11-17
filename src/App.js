2e5555411ddfa47abd7a9d8294e207789b0b534d
JADukes <Jaduke16@gmail.com>
2020-11-17 15:42:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ChessPiece from "./chessPiece";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> yoo wtf</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Router>
+          <ChessPiece />
+          <Route path="/yo" component={ChessPiece} />
+          <Route path="/yo" component={ChessPiece} />
+        </Router>
+        <p className="App-intro">;{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/chessPiece.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class chessPiece extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> load dang it</h1>
+      </div>
+    );
+  }
+}
+
+export default chessPiece;
